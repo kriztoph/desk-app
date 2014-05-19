4ffa6799b0e54a2380b8dbc70f24bff7d6bf7380
Kriztopher Mendoza <kriz@krizinc.com>
2014-05-18 22:09:48 -0700

    javascript file cleanup, combine some files, fix labels, refresh labels list when creating label

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,90 @@
 //= require angular
 //= require angular-resource
 //= require angular-ui-router
-//= require common/services/case
-//= require common/services/filter
-//= require common/services/label
-//= require agent/agent
-//= require cases/cases
-//= require labels/labels
+
+angular.module("deskApp", [
+      'ui.router',
+      'ngResource'
+    ])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+          .state("cases", {
+            url: "/",
+            templateUrl: "/desk/templates/app/cases/cases.html",
+            controller: "CasesCtrl"
+          })
+          .state("labels", {
+            url: "/labels",
+            templateUrl: "/desk/templates/app/labels/labels.html",
+            controller: "LabelsCtrl"
+          })
+
+      $urlRouterProvider.otherwise('/');
+    }])
+
+    .factory('Case', ['$resource', function($resource) {
+      return $resource('/api/v1/cases.json', {}, {
+        get: { method: 'GET', isArray: false }
+      });
+    }])
+
+    .factory('Filter', ['$resource', function($resource) {
+      return $resource('/api/v1/filters.json', {},
+          { 'get':  { method: 'GET', isArray: false },
+            'cases': { method:'GET',
+              params: { filter_id: '@filter_id' },
+              url: '/api/v1/filters/:filter_id/cases' }
+          });
+    }])
+
+    .factory('Label', ['$resource', function($resource) {
+      return $resource('/api/v1/labels.json', {}, {
+        get: { method: 'GET', isArray: false },
+        create: { method: 'POST' }
+      });
+    }])
+
+    .controller('CasesCtrl', ['$scope', 'Case', 'Filter', function($scope, Case, Filter){
+      Case.get().$promise.then(function(result){
+        $scope.cases = result._embedded.entries;
+      });
+
+      Filter.get().$promise.then(function(result){
+        $scope.filters = result._embedded.entries;
+      });
+
+      $scope.filterCases = function(filter){
+        filter_id = filter._links.self.href.replace('/api/v2/filters/', '')
+        Filter.cases({filter_id: filter_id}).$promise.then(function(result){
+          $scope.cases = result._embedded.entries;
+          if ($scope.cases.length == 0) {
+            $scope.no_cases = true;
+          } else {
+            $scope.no_cases = false;
+          }
+        })
+      };
+    }])
+
+    .controller('LabelsCtrl', ['$scope', 'Label', function($scope, Label){
+      $scope.label = {};
+
+      $scope.updateLabels = function(){
+        Label.get().$promise.then(function(result){
+          $scope.labels = result._embedded.entries;
+        });
+      }
+
+      $scope.createLabel = function(){
+        var label = new Label;
+        label.name = $scope.label.name;
+        label.$create().then(function(){
+          $scope.updateLabels();
+        })
+      }
+
+      $scope.updateLabels();
+    }])
+;
 
 
--- app/assets/javascripts/common/services/case.js
+++
@@ -1,8 +0,0 @@
-angular.module('services.case', [])
-    .factory('Case', ['$resource', function($resource) {
-      return $resource('/api/v1/cases.json', {}, {
-        get: { method: 'GET', isArray: false }
-      });
-    }]);
-;
-
