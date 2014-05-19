2e238ebf0572e0db41cbb51ea9b5b787faa799e9
Kriztopher Mendoza <kriz@krizinc.com>
2014-05-18 20:18:27 -0700

    fix syntax for filter angular service so it minifies properly

--- app/assets/javascripts/common/services/filter.js
+++ app/assets/javascripts/common/services/filter.js
@@ -1,10 +1,10 @@
 angular.module('services.filter', [])
-    .factory('Filter', function($resource) {
+    .factory('Filter', ['$resource', function($resource) {
       return $resource('/api/v1/filters.json', {},
       { 'get':  { method: 'GET', isArray: false },
         'cases': { method:'GET',
                    params: { filter_id: '@filter_id' },
                    url: '/api/v1/filters/:filter_id/cases' }
       });
-    })
+    }])
 ;
